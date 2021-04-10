71869f126508fac2faa4aac9d8d505d9e9ef5b25
Marc Walter <walter.marc@outlook.com>
2021-04-10 14:04:09 +0200

    Change config to only build Elm entry points
    See https://github.com/marc136/snowpack-plugin-elm/issues/8#issuecomment-810657940

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,26 @@
+const fg = require('fast-glob');
+
+function excludeEverythingButEntryPoints(entryPoints) {
+  if (process.env.NODE_ENV === 'development') return [];
+
+  if (!Array.isArray(entryPoints)) entryPoints = [entryPoints];
+
+  // If our main entry points are in nested subfolders of 'src/', we can instead use
+  // `fg.sync('src/**/*.elm')` and remove the `concat('*/*.elm')`.
+  return fg
+    .sync('src/*.elm', {
+      ignore: entryPoints.map((v) => `src/${v}`),
+    })
+    .map((v) => v.replace(/^src\//, ''))
+    .concat('*/*.elm');
+}
+
 module.exports = {
   mount: {
     public: '/',
     src: '/_dist_',
   },
+  exclude: excludeEverythingButEntryPoints('Sandbox1.elm'),
   plugins: [['snowpack-plugin-elm', { verbose: true }]],
   devOptions: {
     open: 'none',
